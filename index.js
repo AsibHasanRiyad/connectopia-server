63602807b990138dddf00698b7d2838a1a602aba
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-11-26 16:56:22 +0600

    admin and token verify is added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
@@ -11,8 +12,8 @@ const port = process.env.PORT || 5001;
 //middleware
 
 app.use(cors());
-
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2x6r8lz.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,10 +31,91 @@ async function run() {
     await client.connect();
 
 
+
+    const userCollection = client.db('connectopia').collection('users')
     const postCollection = client.db('connectopia').collection('post')
+    const announcementCollection = client.db('connectopia').collection('announcement')
+
+    //created middleware
+    //verify token
+    const verifyToken = (req, res, next) =>{
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+            return res.status(401).send({message:'unauthorized access'})
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+            if (err) {
+                return res.status(401).send({message:'unauthorized access'})
+            }
+            req.decoded =decoded;
+            next()
+        })
+    }
+
+    //jwt related api
+    app.post ('/jwt', async(req, res) =>{
+        const user = req.body
+        const token= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.send({token})
+    })
+
+
+    //users related api
+    app.get('/users',verifyToken, async(req, res) =>{
+        const email = req.query.email;
+        const query = email ? {email: email} : {}
+        const result = await userCollection.find(query).toArray();
+        res.send(result)
+    })
+    app.get('/users/:email', async(req, res) =>{
+        const email = req.params.email;
+        const query = {email: email } ;
+        const result = await userCollection.findOne(query)
+        res.send(result)
+    })
 
 
 
+
+    app.post('/users', async(req, res) =>{
+        const user = req.body;
+        const query = {email: user?.email}
+        const existingUser = await userCollection.findOne(query)
+        if (existingUser) {
+            return res.send({message: 'users already exist', insertedId:null})
+        }
+        const result = userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    //check admin or not
+    app.get('/users/admin/:email',verifyToken, async (req, res) =>{
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+            return res.status(403).send({message:'forbidden access'})
+        }
+        const query = {email: email}
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+            admin = user?.role === 'admin'
+        }
+        res.send({admin})
+    })
+
+    //make a user admin
+    app.patch('/users/:id', async(req, res) =>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updateDoc = {
+            $set:{
+                role:'admin'
+            }
+        }
+        const result = await userCollection.updateOne(filter, updateDoc)
+        res.send(result)
+    })
 
 
 
@@ -55,6 +137,20 @@ async function run() {
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)}
         const result = await postCollection.findOne(filter)
+        res.send(result)
+    })
+
+
+
+
+    //make announcement api
+    app.get('/announcement', async(req, res) =>{
+        const result = await announcementCollection.find().toArray();
+        res.send(result)
+    })
+    app.post('/announcement', async(req, res) =>{
+        const data = req.body;
+        const result = await announcementCollection.insertOne(data);
         res.send(result)
     })
 
